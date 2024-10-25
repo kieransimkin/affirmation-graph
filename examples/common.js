fb7624df0fd691b7d2971880f12ec7d630656be8
Kieran Simkin <382129+kieransimkin@users.noreply.github.com>
2024-10-25 14:34:37 +0100

    Update to support extended datum

--- examples/common.js
+++ examples/common.js
@@ -1,13 +1,15 @@
-import {Lucid, Blockfrost } from 'lucid-cardano'
-import fs from 'fs'
-export const lucid = await Lucid.new(
+//const {Lucid, Blockfrost } = require('lucid-cardano');
+const fs = require('fs');
+/*exports.lucid = await Lucid.new(
     new Blockfrost("https://cardano-preprod.blockfrost.io/api/v0", "preprod2g0G79mo7zZP2u41GlIVX6e2L19FLcAs"),
     "Preprod",
   );
-  export const skLocation = "examples/credentials/owner.sk"
-  export const addrLocation = "examples/credentials/owner.addr"
-  export const skContent = fs.readFileSync(skLocation).toString();
-  
-  export const ownerWallet = lucid
-  .selectWalletFromSeed(skContent)
+  */
+ exports.skLocation = "examples/credentials/owner.sk"
+ exports.addrLocation = "examples/credentials/owner.addr"
+  exports.skContent = fs.readFileSync(exports.skLocation).toString();
+  /*
+  exports.ownerWallet = lucid
+  .selectWalletFromSeed(exports.skContent)
   .wallet;
+  */
